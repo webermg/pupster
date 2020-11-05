c731d1ead4c7e8640faff8bddcfadc22b9928164
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 11:26:15 -0800

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About'
+import Discover from './components/Discover'
+import Search from './components/Search'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Router>
+
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/blog" component={Discover} />
+          <Route path="/contact" component={Search} />
+        </Switch>
+      </Router>
     </div>
   );
 }
