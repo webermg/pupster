8f396f3e3dc473ff5b8e36da88dd7aae27f80b56
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 11:47:23 -0800

    converted a tags to link components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+  const location = useLocation();
   return (
-    <div>
-      <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pupster</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Discover</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Search</a>
-        </li>
-      </ul>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <span className="navbar-brand mb-0 h1">Navbar</span>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+      <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          About
+        </Link>
+      </li>
+      <li className="nav-item">
+      <Link to="/discover" className={location.pathname === "/discover" ? "nav-link active" : "nav-link"}>
+          Discover
+        </Link>
+      </li>
+      <li className="nav-item">
+      <Link to="/search" className={location.pathname === "/discover" ? "nav-link active" : "nav-link"}>
+          Search
+        </Link>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+    
   )
 }
