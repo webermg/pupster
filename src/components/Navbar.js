968a48014559e82c47a0db6676adff5262459007
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 11:25:43 -0800

    initial

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pupster</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Discover</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Search</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
