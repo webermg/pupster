335f1b127ed35ae6839f449e32c682b34a5b5c91
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 12:10:54 -0800

    page now displays random collection of beagle pictures

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import Api from '../util/Api'
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dogs: []
+    }
+  }
+
+  getDogs = breed => {
+    Api.getBreed(breed).then(data => {
+      // console.log(data)
+      this.setState({
+        dogs: data.data.message
+      })
+    }).catch(err=>console.log(err))
+  }
+
+  componentDidMount() {
+    this.getDogs("beagle")
+  }
+
   render() {
     return (
       <div>
-        Search
+        {this.state.dogs.map(dog => <img src={dog} alt=""/>)}
       </div>
     )
   }
