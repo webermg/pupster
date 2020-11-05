499e00c9ab2964b78dbc3fe7a357ab542be6bf02
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 11:57:43 -0800

    now loads random dog and can cycle through them

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import Api from '../util/Api';
 
 export default class Discover extends Component {
+  constructor() {
+    super()
+    this.state = {
+      current:""
+    }
+  }
+
+  getNewDog = () => {
+    Api.search().then(data => {
+      this.setState({
+        current: data.data.message
+      })
+    }).catch(err=>console.log(err))
+  }
+
+  componentDidMount() {
+    this.getNewDog()
+  }
+
   render() {
     return (
       <div>
-        Discover
+        <img src={this.state.current} alt="" onClick={this.getNewDog}/>
       </div>
     )
   }
