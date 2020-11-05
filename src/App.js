24ad8886e72729ac7cc92d06b1f1212edc8223b8
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 11:47:05 -0800

    fixed routing bugs

--- src/App.js
+++ src/App.js
@@ -9,14 +9,18 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      
       <Router>
-
+      <Navbar />
         <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/blog" component={Discover} />
-          <Route path="/contact" component={Search} />
+          <Route exact path="/discover">
+            <Discover/>
+          </Route>
+          <Route path="/search">
+            <Search/>
+          </Route> 
         </Switch>
       </Router>
     </div>
