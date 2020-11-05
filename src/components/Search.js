2ecbfaf54ea9206a12e77c22f0e534c6678d641a
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 12:57:18 -0800

    added search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,9 @@ export default class Search extends Component {
   constructor() {
     super();
     this.state = {
-      dogs: []
+      dogs: [],
+      breeds: [],
+      breed: ""
     }
   }
 
@@ -18,14 +20,54 @@ export default class Search extends Component {
     }).catch(err=>console.log(err))
   }
 
+  getBreeds = () => {
+    Api.getBreeds().then(data => {
+      // console.log(data.data.message)
+      this.setState({
+        breeds: Object.keys(data.data.message)
+      })
+    }).catch(err=>console.log(err))
+  }
+
   componentDidMount() {
-    this.getDogs("beagle")
+    this.getBreeds()
+  }
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.getDogs(this.state.breed);
+    this.setState({
+      breed: ""
+    })
   }
 
   render() {
     return (
-      <div>
-        {this.state.dogs.map(dog => <img src={dog} alt=""/>)}
+      <div className="container">
+        <h1>Search By Breed</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label for="breedInput">Dog Breed</label>
+            <input name="breed" type="text" list="breeds" className="form-control" id="breedInput" placeholder="" onChange={this.handleInputChange}/>
+            <button className="btn btn-success">Search</button>
+            <datalist id="breeds">
+              {this.state.breeds.map(breed => <option value={breed}></option>)}
+            </datalist>
+          </div>
+        </form>
+        <ul className="list-group">
+          {this.state.dogs.map(dog => <li className="list-group-item"><img className="img-responsive" src={dog} alt=""/></li>)}
+        </ul>
       </div>
     )
   }
