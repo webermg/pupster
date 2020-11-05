24559a14b54d681d8447c9cb02818abe95a893d8
webermg <matthew.gerald.weber@gmail.com>
2020-11-05 12:00:37 -0800

    added function to get all from breed

--- src/util/Api.js
+++ src/util/Api.js
@@ -5,7 +5,11 @@ const RANDOM = "breeds/image/random";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: function(query) {
+  getRandom: function() {
     return axios.get(BASEURL + RANDOM);
+  },
+  getBreed: function(breed) {
+    const query = BASEURL + `breed/${breed}/images`
+    return axios.get(BASEURL + query);
   }
 };
